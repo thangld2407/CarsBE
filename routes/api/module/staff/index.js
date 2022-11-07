cfa4f4241c4c0d4c64b5acfebbfa54708d788e1e
thangld <thangld2407@gmail.com>
2022-11-07 22:35:46 +0700

    update query

--- routes/api/module/staff/index.js
+++ routes/api/module/staff/index.js
@@ -5,8 +5,8 @@ const staffRouter = require('express').Router();
 
 staffRouter.post('/create', requireLogin, StaffController.create);
 staffRouter.post('/edit', requireLogin, StaffController.edit);
-staffRouter.post('/remove', requireLogin, StaffController.remove);
+staffRouter.post('/remove', StaffController.remove);
 staffRouter.post('/list', StaffController.list);
-staffRouter.get('/:id', requireLogin, StaffController.detail);
+staffRouter.get('/:id', StaffController.detail);
 
 module.exports = staffRouter;

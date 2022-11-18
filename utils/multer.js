3383f376144ddef808bedbbdfb2080f5ec08b0f2
Winx - Broken <thangld2407@gmail.com>
2022-11-18 15:44:34 +0700

    change file name

--- utils/multer.js
+++ utils/multer.js
@@ -1,11 +1,19 @@
 const multer = require('multer');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+let filename = '';
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
 		cb(null, path.join(__dirname, '../public/uploads'));
 	},
 	filename: function (req, file, cb) {
-		cb(null, `uploads-${Date.now()}.${file.mimetype.split('/')[1]}`);
+		if (file.mimetype.includes('image')) {
+			filename = `uploads-${Date.now()}-${uuidv4()}.webp`;
+		} else {
+			filename = `uploads-${Date.now()}-${uuidv4()}.${file.mimetype.split('/')[1]}`;
+		}
+		cb(null, filename);
 	}
 });
 
@@ -28,5 +36,7 @@ const filter = (req, file, cb) => {
 	cb('Please upload only image', false);
 };
 
-const uploadFile = multer({ storage: storage });
+const uploadFile = multer({
+	storage: storage
+});
 module.exports = uploadFile;

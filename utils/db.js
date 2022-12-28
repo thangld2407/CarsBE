afd7dbd92317427030477abfb6772d123d2627a3
thangld <thangld2407@gmail.com>
2022-12-28 21:09:27 +0700

    update db config

--- utils/db.js
+++ utils/db.js
@@ -1,15 +1,29 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connOptions = {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
 	//   useCreateIndex: true,
 };
-const MONGO_URI = process.env.MONGO_URI;
+
+const db = {
+	connect: process.env.DB_CONNECTION,
+	host: process.env.DB_HOST,
+	database: process.env.DB_DATABASE,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD
+};
+
+const auth = db.user && db.password ? `${db.user}:${db.password}@` : '';
 
 const connectToDB = async () => {
 	try {
-		const connect = await mongoose.connect(MONGO_URI, connOptions);
+		const connect = await mongoose.connect(`${db.connect}://${auth}${db.host}/${db.database}`, {
+			...connOptions,
+			connectTimeoutMS: 10000,
+			socketTimeoutMS: 45000
+		});
 		if (connect) console.log(`Mongodb connected - ${connect.connection.host}`);
 	} catch (err) {
 		console.log(`Database error ${err}`);
